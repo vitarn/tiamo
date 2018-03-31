bc4abb7ea1e174946ebd91cdd656cf225235a9d7
colder <colder@vitarn.com>
2018-04-01 03:54:12 +0800

    refactor(index): remove ModelProperties
    refactor(index): export Schema from tdv

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 export const { createSet } = DocumentClient.prototype
-export { Model, ModelProperties } from './lib/model'
+export { Model } from './lib/model'
 export * from './lib/decorator'
+export { Schema, SchemaOptions, SchemaProperties } from 'tdv'
